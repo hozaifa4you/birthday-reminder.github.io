b4e4da6b662856a30abfc27b0b42219854e5553f
yousuf4you <yousuf360.ya@gmail.com>
2021-01-30 18:55:11 +0600

    just for deployment persas

---
+++ precache-manifest.0e426b86a171baeb5d572c21e19393f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348a9ed1134ac7838e1521be79467461",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4ec546f2757cb125f97",
+    "url": "/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4ec546f2757cb125f97",
+    "url": "/static/js/main.daf200ad.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
